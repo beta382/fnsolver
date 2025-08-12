d98743208bc574ac0faf4712be358607b51c8bf2
Dan Keenan <dk@dankeenan.org>
2025-08-12 18:46:45 -0400

    Support Qt 6.4

--- fnsolver/gui/main_window.cpp
+++ fnsolver/gui/main_window.cpp
@@ -1,6 +1,5 @@
 #include "main_window.h"
 
-#include <iostream>
 #include <thread>
 #include <QApplication>
 #include <QMenuBar>
@@ -20,6 +19,7 @@
 #include "run_dialog.h"
 #include "settings.h"
 #include "fnsolver/fnsolver_config.h"
+#include "qicon_from_theme.h"
 
 MainWindow::MainWindow(QWidget* parent): QMainWindow(parent), solver_options_(options_loader::default_options()),
   layout_(fill_layout(solver_options_.get_seed(), solver_options_.get_locked_sites())) {
@@ -138,25 +138,25 @@ void MainWindow::init_ui() {
 void MainWindow::init_actions() {
   // File
   // New
-  actions.file_new = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DocumentNew), tr("&New"), this);;
+  actions.file_new = new QAction(qicon_from_theme(ThemeIcon::DocumentNew), tr("&New"), this);;
   actions.file_new->setShortcut(QKeySequence(QKeySequence::New));
   connect(actions.file_new, &QAction::triggered, this, &MainWindow::file_new);
   // Open
   actions.file_open =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DocumentOpen), tr("&Open"), this);
+    new QAction(qicon_from_theme(ThemeIcon::DocumentOpen), tr("&Open"), this);
   actions.file_open->setShortcut(QKeySequence::Open);
   connect(actions.file_open, &QAction::triggered, this, &MainWindow::file_open);
   // Recent
   actions.file_recent = new QMenu(tr("Recent Documents"), this);
-  actions.file_recent->setIcon(QIcon::fromTheme(QIcon::ThemeIcon::DocumentOpenRecent));
+  actions.file_recent->setIcon(qicon_from_theme(ThemeIcon::DocumentOpenRecent));
   update_recent_documents();
   // Save
   actions.file_save =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DocumentSave), tr("&Save"), this);
+    new QAction(qicon_from_theme(ThemeIcon::DocumentSave), tr("&Save"), this);
   actions.file_save->setShortcut(QKeySequence::Save);
   connect(actions.file_save, &QAction::triggered, this, &MainWindow::file_save);
   // Save As
-  actions.file_save_as = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DocumentSaveAs),
+  actions.file_save_as = new QAction(qicon_from_theme(ThemeIcon::DocumentSaveAs),
                                      tr("Save &As"), this);
   actions.file_save_as->setShortcut(QKeySequence::SaveAs);
   connect(actions.file_save_as, &QAction::triggered, this,
@@ -165,39 +165,39 @@ void MainWindow::init_actions() {
   actions.file_frontiernav_url = new QAction(tr("Open in FrontierNav.net..."), this);
   connect(actions.file_frontiernav_url, &QAction::triggered, this, &MainWindow::file_frontiernav_url);
   // Exit
-  actions.file_exit = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::ApplicationExit),
+  actions.file_exit = new QAction(qicon_from_theme(ThemeIcon::ApplicationExit),
                                   tr("E&xit"), this);
   connect(actions.file_exit, &QAction::triggered, this, &MainWindow::close);
 
   // View
   // Zoom In
   actions.view_zoom_in =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::ZoomIn), tr("Zoom &In"), this);
+    new QAction(qicon_from_theme(ThemeIcon::ZoomIn), tr("Zoom &In"), this);
   actions.view_zoom_in->setShortcut(QKeySequence::ZoomIn);
   // Zoom Out
   actions.view_zoom_out =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::ZoomOut), tr("Zoom &Out"), this);
+    new QAction(qicon_from_theme(ThemeIcon::ZoomOut), tr("Zoom &Out"), this);
   actions.view_zoom_out->setShortcut(QKeySequence::ZoomOut);
   // Zoom All
-  actions.view_zoom_all = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::ZoomFitBest),
+  actions.view_zoom_all = new QAction(qicon_from_theme(ThemeIcon::ZoomFitBest),
                                       tr("Zoom &All"), this);
   actions.view_zoom_all->setShortcut(QKeySequence::SelectAll);
 
   // Help
   // About
   actions.help_about =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::HelpAbout), tr("About"), this);
+    new QAction(qicon_from_theme(ThemeIcon::HelpAbout), tr("About"), this);
   connect(actions.help_about, &QAction::triggered, this, &MainWindow::help_about);
   // Show website
   actions.help_website =
-    new QAction(QIcon::fromTheme(QIcon::ThemeIcon::HelpFaq), tr("Website"), this);
+    new QAction(qicon_from_theme(ThemeIcon::HelpFaq), tr("Website"), this);
   actions.help_website->setShortcut(QKeySequence::HelpContents);
   connect(actions.help_website, &QAction::triggered, this,
           &MainWindow::help_website);
 
   // Run Simulation
   actions.run_simulation = new QAction(
-    QIcon::fromTheme(QIcon::ThemeIcon::MediaPlaybackStart), tr("Solve"), this);
+    qicon_from_theme(ThemeIcon::MediaPlaybackStart), tr("Solve"), this);
   actions.run_simulation->setShortcut(QKeySequence::Refresh);
   connect(actions.run_simulation, &QAction::triggered, this, &MainWindow::solve);
 }
